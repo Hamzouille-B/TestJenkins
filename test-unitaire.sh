a0ace338b7c54d6362f9b5b9a26171a2e9e42532
Hamza Benoit <54987518+Hamzouille-B@users.noreply.github.com>
2023-05-31 15:15:04 +0200

    Update test-unitaire.sh

--- test-unitaire.sh
+++ test-unitaire.sh
@@ -3,7 +3,7 @@
 # ===============
 # Test le script suite avec les valeurs 1, 2, 3 et vérifier que le résultat est bien 5
 echo "test unitaire du script suite.sh"
-resultat=$(./suite.sh 1 2 3)
+resultat=$(/home/mehdi/projetgit/TestJenkins/calcul.sh 1 2 3)
 
 if [[ $resultat -eq 5 ]]
 then
